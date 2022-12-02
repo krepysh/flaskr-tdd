cfabe96980b5e26f9a0024a4c4aaf29e90f9cb8e
Anton Topchii <anton.topchii@getbyrd.com>
2022-12-02 20:06:20 +0100

    Add: login/logout/index views and tests

--- project/app.py
+++ project/app.py
@@ -1,26 +1,86 @@
+import os
 import sqlite3
+from sqlite3 import Connection
+from typing import NamedTuple
 
-from flask import Flask, g
+from flask import Flask, g, render_template, session, request, flash, redirect, url_for, abort
 
-DATABASE = 'flaskr.sqlite3'
+DATABASE = "flaskr.sqlite3"
+USER_NAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 app = Flask(__name__)
 
 app.config.from_object(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route("/")
+def index():
+    db = get_db()
+    cur = db.execute("select * from entries order by id desc")
+    entries = cur.fetchall()
+    return render_template("index.html", entries=entries)
 
 
-def connect_db():
-    connector = sqlite3.connect(app.config['DATABASE'])
+def validate_user(username, password) -> bool:
+    return username == app.config["USER_NAME"] and password == app.config["PASSWORD"]
+
+
+def user_login():
+    session["logged_in"] = True
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        if validate_user(request.form["username"], request.form["password"]):
+            user_login()
+            return redirect(url_for("index"))
+        error = "User not found."
+        flash(error)
+    return render_template("login.html")
+
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    session.pop("logged_in", None)
+    flash("You've logged out.")
+    return redirect(url_for("index"))
+
+
+class Entry(NamedTuple):
+    title: str
+    body: str
+
+
+def extract_entry(form: dict) -> Entry:
+    title = form["title"]
+    body = form["body"]
+    return Entry(title=title, body=body)
+
+
+@app.route("/add_entry", methods=["POST"])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    entry = extract_entry(request.form)
+    db = get_db()
+    query = "INSERT into entries (title, body) VALUES (?, ?);"
+    cur = db.execute(query, entry)
+    db.commit()
+    flash(f"Inserted {cur.rowcount} rows.")
+    return redirect(url_for("index"))
+
+
+def connect_db() -> Connection:
+    connector = sqlite3.connect(app.config["DATABASE"])
     connector.row_factory = sqlite3.Row
     return connector
 
 
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
+def get_db() -> Connection:
+    if not hasattr(g, "sqlite_db"):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
@@ -28,7 +88,7 @@ def get_db():
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', 'r') as f:
+        with app.open_resource("schema.sql", "r") as f:
             cursor = db.cursor()
             cursor.executescript(f.read())
         db.commit()
@@ -37,9 +97,9 @@ def init_db():
 
 @app.teardown_appcontext
 def close_db(error):
-    if hasattr(g, 'sqlite_db'):
+    if hasattr(g, "sqlite_db"):
         g.sqlite_db.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,65 @@
+import pytest
+from flask.testing import FlaskClient
+
 from project.app import app, init_db
 from pathlib import Path
 
-def test_app():
-    client = app.test_client()
 
-    response = client.get('/', content_type='html/text')
+@pytest.fixture
+def client():
+    app.config["DATABASE"] = "test_db.sqlite3"
+    app.config["TESTING"] = True
 
+    init_db()
+    yield app.test_client()
+    init_db()
+
+
+def login(client: FlaskClient):
+    username = app.config["USER_NAME"]
+    password = app.config["PASSWORD"]
+
+    return client.post(
+        "/login", data=dict(username=username, password=password), follow_redirects=True
+    )
+
+
+def logout(client: FlaskClient):
+    return client.post("/logout", follow_redirects=True)
+
+
+@pytest.fixture
+def logged_in_client(client: FlaskClient) -> FlaskClient:
+    login(client)
+    return client
+
+
+def test_index(client):
+    response = client.get("/", content_type="html/text")
     assert response.status_code == 200
-    assert response.data == b'Hello World!'
+
+
+def test_login_logout(client):
+    res = login(client)
+    assert res.status_code == 200
+    logout_result = logout(client)
+    assert logout_result.status_code == 200
 
 
 def test_db():
     init_db()
-    assert Path('flaskr.sqlite3').is_file()
+    assert Path("tests/test_db.sqlite3").is_file()
+
+
+def test_empty_db(client):
+    response = client.get("/")
+    assert b"No messages, yet. Add one." in response.data
+
+
+def test_add_entry(logged_in_client):
+    response = logged_in_client.post(
+        "/add_entry", data=dict(title="ttitle", body="tbody"), follow_redirects=True
+    )
+    assert response.status_code == 200
+    assert b"ttitle" in response.data
+    assert b"tbody" in response.data
